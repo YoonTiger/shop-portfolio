20d70ef89fbfbba65680276e952a7b71ec3f5f0c
YoonTiger <yuno110@hanmail.net>
2022-06-22 19:44:44 +0900

    d

--- src/main/java/basic/shop/exceptions/ExHandler.java
+++ src/main/java/basic/shop/exceptions/ExHandler.java
@@ -1,15 +1,18 @@
 package basic.shop.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Slf4j
 @ControllerAdvice
 public class ExHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
     public String exHandle(Exception e, Model model){
 
